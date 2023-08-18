2e9e6e97dde5d73c6cd7cf1e6777b744eec8ff4e
Patrick Melo <patrickabimael@yahoo.com.br>
2023-08-18 19:07:27 -0300

    First Commit

---
+++ src/main/java/saboroso/saborosoburguer/SaborosoburguerApplication.java
@@ -0,0 +1,13 @@
+package saboroso.saborosoburguer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaborosoburguerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaborosoburguerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/saboroso/saborosoburguer/SaborosoburguerApplicationTests.java
@@ -0,0 +1,13 @@
+package saboroso.saborosoburguer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaborosoburguerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
