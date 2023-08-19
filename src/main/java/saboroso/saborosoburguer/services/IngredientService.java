78c5ce420fe6024ebb67635954ce6ec39c09d722
Patrick Melo <patrickabimael@yahoo.com.br>
2023-08-19 19:24:30 -0300

    implements boolean value for successful query

--- src/main/java/saboroso/saborosoburguer/services/IngredientService.java
+++ src/main/java/saboroso/saborosoburguer/services/IngredientService.java
@@ -13,19 +13,23 @@ public class IngredientService {
     public IngredientService(IngredientRepository ingredientRepository) {
         this.ingredientRepository = ingredientRepository;
     }
-    void addIngredient (inputIngredientDTO ingredientDTO) {
+    public Boolean insertIngredient (inputIngredientDTO ingredientDTO) {
+        if (ingredientRepository.existsByTitleAndGrams(ingredientDTO.title(), ingredientDTO.grams())) return false;
         Ingredient newIngredient = new Ingredient(ingredientDTO);
         ingredientRepository.save(newIngredient);
+        return true;
     }
     public List<Ingredient> getIngredients () {
         return ingredientRepository.findAll();
     }
-    void editIngredient(inputIngredientDTO changes, String identifier) {
+    public Boolean editIngredient(inputIngredientDTO changes, String identifier) {
+        if (ingredientRepository.existsByTitleAndGrams(changes.title(), changes.grams())) return false;
         Ingredient editedIngredient = ingredientRepository.findByIdentifier(identifier);
         if (changes.grams() != null) editedIngredient.setGrams(changes.grams());
         if (changes.inStock() != null) editedIngredient.setGrams(changes.grams());
         if (changes.title() != null) editedIngredient.setTitle(changes.title());
         ingredientRepository.save(editedIngredient);
+        return true;
     }
     public Boolean removeIngredient(String identifier){
         Ingredient deletedIngredient = ingredientRepository.findByIdentifier(identifier);
