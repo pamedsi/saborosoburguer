d425832efe19c1e7c1b45a448650f782785069b6
Patrick Melo <patrickabimael@yahoo.com.br>
2023-08-19 19:37:13 -0300

    implements temporary getter for ingredients

--- src/main/java/saboroso/saborosoburguer/controllers/IngredientController.java
+++ src/main/java/saboroso/saborosoburguer/controllers/IngredientController.java
@@ -23,9 +23,14 @@ public class IngredientController {
         if (ingredientService.insertIngredient(ingredientDTO)) return ResponseEntity.ok(new Message(ingredientDTO.title() + " adicionado!"));
         return ResponseEntity.status(HttpStatus.CONFLICT).body(new Message(ingredientDTO.title() + " já adicionado."));
     }
+    @GetMapping
+    @RequestMapping (value = "/get-ingredients")
+    public ResponseEntity<?> getIngredients() {
+        return ResponseEntity.ok(ingredientService.getIngredients());
+    }
 
     @PostMapping
-    @RequestMapping (value = "/insert-ingredient")
+    @RequestMapping (value = "/update-ingredient")
     public ResponseEntity<?> updateIngredient(@Valid @RequestBody inputIngredientDTO changes, @PathVariable String identifier) {
         if (ingredientService.editIngredient(changes, identifier)) return ResponseEntity.ok(new Message(changes.title() + " atualizado!"));
         return ResponseEntity.status(HttpStatus.CONFLICT).body(new Message(changes.title() + " já tem esses dados."));
