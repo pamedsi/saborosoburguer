12f7ef494df05ad3fecdecf6279a38d6b38dff81
Patrick Melo <patrickabimael@yahoo.com.br>
2023-08-19 18:40:30 -0300

    edits ingredient DTO

--- src/main/java/saboroso/saborosoburguer/DTOs/inputIngredientDTO.java
+++ src/main/java/saboroso/saborosoburguer/DTOs/inputIngredientDTO.java
@@ -1,11 +1,14 @@
 package saboroso.saborosoburguer.DTOs;
 
 import jakarta.validation.constraints.NotBlank;
+import jdk.jfr.BooleanFlag;
 import org.springframework.format.annotation.NumberFormat;
 
 public record inputIngredientDTO(
         @NotBlank
         String title,
         @NumberFormat
-        Integer grams
+        Integer grams,
+        @BooleanFlag
+        Boolean inStock
 ) {}
