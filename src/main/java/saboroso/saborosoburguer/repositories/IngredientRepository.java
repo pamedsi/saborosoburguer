5a0acbad01f0b4552831058ec76baf5dc5e0d86e
Patrick Melo <patrickabimael@yahoo.com.br>
2023-08-19 18:45:08 -0300

    implements findByIdentifier method

--- src/main/java/saboroso/saborosoburguer/repositories/IngredientRepository.java
+++ src/main/java/saboroso/saborosoburguer/repositories/IngredientRepository.java
@@ -1,7 +1,12 @@
 package saboroso.saborosoburguer.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
 import saboroso.saborosoburguer.entities.Ingredient;
 
+@Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+    Ingredient findByIdentifier(String identifier);
 }
