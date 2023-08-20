5848b707a97052a2417d85c1505e5567b3c5d883
Patrick Melo <patrickabimael@yahoo.com.br>
2023-08-20 00:31:00 -0300

    implements constructor for burger entity

--- src/main/java/saboroso/saborosoburguer/entities/Burger.java
+++ src/main/java/saboroso/saborosoburguer/entities/Burger.java
@@ -1,23 +1,31 @@
 package saboroso.saborosoburguer.entities;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+import saboroso.saborosoburguer.DTOs.burger.InputBurgerDTO;
 import saboroso.saborosoburguer.model.BurgerCategory;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
+import java.util.UUID;
 
 @Entity
+@Data
 public class Burger {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private Long id;
-    @GeneratedValue (strategy = GenerationType.UUID)
+    @Setter(AccessLevel.NONE)
     private String identifier;
     @Column
     private BurgerCategory category;
-    @Column (unique = true)
+    @Column (unique = true, nullable = false)
     private String title;
-    @Column
+    @Column (nullable = false)
     private BigDecimal price;
     @Column (length = 2000)
     private String pic;
@@ -28,4 +36,13 @@ public class Burger {
             joinColumns = @JoinColumn(name = "burger_id"),
             inverseJoinColumns = @JoinColumn(name = "ingredient_id"))
     private List<Ingredient> ingredients;
+    public Burger(InputBurgerDTO inputBurger) {
+        identifier = UUID.randomUUID().toString();
+        category = BurgerCategory.valueOf(inputBurger.category().toUpperCase().replace(" ", "_"));
+        title = inputBurger.title();
+        price = inputBurger.price().setScale(2, RoundingMode.HALF_UP);
+        pic = inputBurger.pic();
+        if (inputBurger.inStock() != null) inStock = inputBurger.inStock();
+        else inStock = true;
+    }
 }
