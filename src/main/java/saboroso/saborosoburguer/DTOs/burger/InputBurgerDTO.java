b6c140c89f5992b8538d79ed93ae5727e42efe6b
Patrick Melo <patrickabimael@yahoo.com.br>
2023-08-20 00:30:42 -0300

    implements validator for input of burgers

---
+++ src/main/java/saboroso/saborosoburguer/DTOs/burger/InputBurgerDTO.java
@@ -0,0 +1,19 @@
+package saboroso.saborosoburguer.DTOs.burger;
+
+import jdk.jfr.BooleanFlag;
+import org.springframework.format.annotation.NumberFormat;
+import saboroso.saborosoburguer.validations.burger.ValidBurgerCategory;
+
+import java.math.BigDecimal;
+
+public record InputBurgerDTO(
+        @ValidBurgerCategory
+        String category,
+        String title,
+        @NumberFormat
+        BigDecimal price,
+        String pic,
+        @BooleanFlag
+        Boolean inStock
+)
+{}
