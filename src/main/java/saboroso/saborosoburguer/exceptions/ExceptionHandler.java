198797484c31b2a5c03114996f3368cbe87ebe67
Patrick Melo <patrickabimael@yahoo.com.br>
2023-09-01 19:46:06 -0300

    creates non-functional exception handler

---
+++ src/main/java/saboroso/saborosoburguer/exceptions/ExceptionHandler.java
@@ -0,0 +1,12 @@
+package saboroso.saborosoburguer.exceptions;
+
+import org.springframework.security.authentication.BadCredentialsException;
+
+public class ExceptionHandler {
+
+    public IllegalArgumentException illegalArgumentException () {
+        return null;
+    }
+
+    public BadCredentialsException badCredentialsException () { return null; }
+}
