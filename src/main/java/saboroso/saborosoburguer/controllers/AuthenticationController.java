a031683ce21c7e81ea1fa66a8fde553ece298063
Patrick Melo <patrickabimael@yahoo.com.br>
2023-09-01 22:38:00 -0300

    implements AuthenticationController

--- src/main/java/saboroso/saborosoburguer/controllers/AuthenticationController.java
+++ src/main/java/saboroso/saborosoburguer/controllers/AuthenticationController.java
@@ -4,25 +4,30 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import saboroso.saborosoburguer.DTOs.LoginDTO;
+import saboroso.saborosoburguer.DTOs.TokenDTO;
+import saboroso.saborosoburguer.entities.UserEntity;
 import saboroso.saborosoburguer.model.BaseController;
-import saboroso.saborosoburguer.model.Message;
+import saboroso.saborosoburguer.security.Token;
 
 @RestController
 public class AuthenticationController extends BaseController {
     private final AuthenticationManager authenticationManager;
+    private final Token tokenService;
 
-    public AuthenticationController(AuthenticationManager authenticationManager) {
+    public AuthenticationController(AuthenticationManager authenticationManager, Token token) {
         this.authenticationManager = authenticationManager;
+        tokenService = token;
     }
     @PostMapping ("/login")
     public ResponseEntity<?> login(@RequestBody @Valid LoginDTO login) {
         UsernamePasswordAuthenticationToken username = new UsernamePasswordAuthenticationToken(login.email(), login.password());
-        authenticationManager.authenticate(username);
-
-        return ResponseEntity.ok(new Message("Bem-vindo!"));
+        Authentication auth = authenticationManager.authenticate(username);
+        String token = tokenService.generateToken((UserEntity) auth.getPrincipal());
+        return ResponseEntity.ok(new TokenDTO(token));
     }
 }
