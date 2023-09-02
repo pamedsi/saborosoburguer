1b45d7a1624df0c6206518ddcfd8103fda99af85
Patrick Melo <patrickabimael@yahoo.com.br>
2023-09-01 22:40:24 -0300

    updates ExceptionHandler for forthcoming implementations

--- src/main/java/saboroso/saborosoburguer/exceptions/ExceptionHandler.java
+++ src/main/java/saboroso/saborosoburguer/exceptions/ExceptionHandler.java
@@ -1,12 +1,15 @@
 package saboroso.saborosoburguer.exceptions;
 
+import org.springframework.context.ApplicationContextException;
 import org.springframework.security.authentication.BadCredentialsException;
 
 public class ExceptionHandler {
 
-    public IllegalArgumentException illegalArgumentException () {
+    public IllegalArgumentException illegalArgumentExceptionHandler () {
         return null;
     }
-
-    public BadCredentialsException badCredentialsException () { return null; }
+    public BadCredentialsException badCredentialsExceptionHandler () { return null; }
+    public RuntimeException runtimeExceptionHandler() { return null; }
+    public ApplicationContextException applicationContextExceptionHandler() { return null; }
+    public NullPointerException nullPointerExceptionHandler() { return null; }
 }
