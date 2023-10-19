113c57a4dd20168c41682d91406cac641da829c9
Patrick Melo <patrickabimael@yahoo.com.br>
2023-10-18 21:33:45 -0300

    adds exception for future handling

--- src/main/java/saboroso/saborosoburguer/exceptions/HandlerForExceptions.java
+++ src/main/java/saboroso/saborosoburguer/exceptions/HandlerForExceptions.java
@@ -2,12 +2,15 @@ package saboroso.saborosoburguer.exceptions;
 
 import org.hibernate.sql.ast.tree.from.UnknownTableReferenceException;
 import org.springframework.context.ApplicationContextException;
+import org.springframework.core.convert.ConverterNotFoundException;
 import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-public class ExceptionHandler {
+//@ExceptionHandler
+public class HandlerForExceptions {
 
     public IllegalArgumentException illegalArgumentExceptionHandler () {
         return null;
@@ -22,4 +25,5 @@ public class ExceptionHandler {
     public UnsupportedOperationException unsupportedOperationException() { return null; }
     public HttpRequestMethodNotSupportedException httpRequestMethodNotSupportedException () { return null; }
     public UnknownTableReferenceException unknownTableReferenceException() { return null; }
+    public ConverterNotFoundException converterNotFoundException() { return null; }
 }
