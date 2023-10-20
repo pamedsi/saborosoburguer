31402a4ea89c89df4ae2fb3fb354513f2aeb7362
Patrick Melo <patrickabimael@yahoo.com.br>
2023-10-20 16:26:27 -0300

    inherits from another class

--- src/main/java/saboroso/saborosoburguer/entities/soldItems/BurgerSale.java
+++ src/main/java/saboroso/saborosoburguer/entities/soldItems/BurgerSale.java
@@ -1,27 +1,48 @@
 package saboroso.saborosoburguer.entities.soldItems;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import saboroso.saborosoburguer.entities.CustomerOrder;
+import saboroso.saborosoburguer.entities.menuItems.accompaniment.Combo;
 import saboroso.saborosoburguer.entities.menuItems.burger.Burger;
 import saboroso.saborosoburguer.entities.menuItems.burger.BurgerBread;
+import saboroso.saborosoburguer.entities.soldItems.accompaniment.AddOnSale;
+import saboroso.saborosoburguer.entities.soldItems.accompaniment.ComboSale;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table
 @NoArgsConstructor
-public class BurgerSale extends SoldItem{
+@Getter
+public class BurgerSale extends MenuItemSale {
     @ManyToOne
     @JoinColumn(name = "burger_id")
     private Burger soldburger;
     @ManyToOne
     @JoinColumn (name = "bread_id")
     private BurgerBread bread;
+    @ManyToOne
+    @JoinColumn (name = "combo_sale_id")
+    private ComboSale combo;
+    @OneToMany
+    @JoinTable (
+            name = "burger_add_on_sale",
+            joinColumns = @JoinColumn(name = "burger_id"),
+            inverseJoinColumns = @JoinColumn(name = "add_on_sale_id")
+    )
+    private List<AddOnSale> addOns;
 
-    public BurgerSale(CustomerOrder orderThatSold, Burger soldburger, BurgerBread bread) {
+    public BurgerSale(CustomerOrder orderThatSold, Burger soldburger, BurgerBread bread, ComboSale combo, List<AddOnSale> addOns, String obs) {
         this.soldburger = soldburger;
         this.bread = bread;
+        this.combo = combo;
+        this.addOns = addOns;
+        setObs(obs);
         setOrderThatSold(orderThatSold);
-        setSingleUnitySoldFor(soldburger.getPrice());
+        setSoldFor(soldburger.getPrice());
     }
 }
