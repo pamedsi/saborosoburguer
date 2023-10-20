ab514ef1dd53e5444d5ec03860c2c2bbbe25df9e
Patrick Melo <patrickabimael@yahoo.com.br>
2023-10-20 16:23:16 -0300

    uses heritage for menu item sale

---
+++ src/main/java/saboroso/saborosoburguer/entities/soldItems/MenuItemSale.java
@@ -0,0 +1,19 @@
+package saboroso.saborosoburguer.entities.soldItems;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import saboroso.saborosoburguer.entities.CustomerOrder;
+import saboroso.saborosoburguer.entities.soldItems.accompaniment.BaseAccompanimentSale;
+@Getter
+@Setter
+@Entity
+@Inheritance(strategy= InheritanceType.TABLE_PER_CLASS)
+public abstract class MenuItemSale extends BaseAccompanimentSale {
+    @ManyToOne (fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "client_order_id")
+    private CustomerOrder orderThatSold;
+
+    @Column
+    private String obs;
+}
