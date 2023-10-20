4032e9be05b4094c9a55e5a3bb2743275e138a8a
Patrick Melo <patrickabimael@yahoo.com.br>
2023-10-20 16:21:49 -0300

    uses relationship one to many with add-on

--- src/main/java/saboroso/saborosoburguer/entities/soldItems/PortionSale.java
+++ src/main/java/saboroso/saborosoburguer/entities/soldItems/PortionSale.java
@@ -5,18 +5,29 @@ import lombok.NoArgsConstructor;
 
 import saboroso.saborosoburguer.entities.CustomerOrder;
 import saboroso.saborosoburguer.entities.menuItems.Portion;
+import saboroso.saborosoburguer.entities.soldItems.accompaniment.AddOnSale;
+
+import java.util.List;
 
 @Entity
 @Table
 @NoArgsConstructor
-public class PortionSale extends SoldItem {
+public class PortionSale extends MenuItemSale {
     @ManyToOne
     @JoinColumn(name = "portion_id")
     private Portion soldPortion;
+    @OneToMany
+    @JoinTable (
+            name = "portion_add_on_sale",
+            joinColumns = @JoinColumn(name = "portion_id"),
+            inverseJoinColumns = @JoinColumn(name = "add_on_sale_id")
+    )
+    private List<AddOnSale> addOns;
 
-    public PortionSale(CustomerOrder orderThatSold, Portion soldPortion) {
+    public PortionSale(CustomerOrder orderThatSold, Portion soldPortion, List<AddOnSale> addOns, String obs) {
         this.soldPortion = soldPortion;
+        this.addOns = addOns;
         setOrderThatSold(orderThatSold);
-        setSingleUnitySoldFor(soldPortion.getPrice());
+        setSoldFor(soldPortion.getPrice());
     }
 }
