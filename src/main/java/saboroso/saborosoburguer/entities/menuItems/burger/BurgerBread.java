bc8fd58dcb7922747dfad593c07e69e8da52199b
Patrick Melo <patrickabimael@yahoo.com.br>
2023-10-22 14:45:07 -0300

    implements pic prop

--- src/main/java/saboroso/saborosoburguer/entities/menuItems/burger/BurgerBread.java
+++ src/main/java/saboroso/saborosoburguer/entities/menuItems/burger/BurgerBread.java
@@ -1,14 +1,20 @@
 package saboroso.saborosoburguer.entities.menuItems.burger;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import saboroso.saborosoburguer.DTO.burger.BurgerBreadDTO;
 
 @Entity
 @Table
 @NoArgsConstructor
+@Getter
 public class BurgerBread extends BurgerComponent{
+    @Column (length = 2000)
+    @Getter
+    private String pic;
     public BurgerBread(BurgerBreadDTO burgerBreadDTO) {
         setTitle(burgerBreadDTO.title());
         setInStock(burgerBreadDTO.inStock());
