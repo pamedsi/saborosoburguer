cc0a5e361faf4e2dc2bbae87ce6423bcdb99d499
Patrick Melo <patrickabimael@yahoo.com.br>
2023-10-22 14:43:19 -0300

    renames prop to "singleUnitySoldFor"

--- src/main/java/saboroso/saborosoburguer/entities/soldItems/DrinkSale.java
+++ src/main/java/saboroso/saborosoburguer/entities/soldItems/DrinkSale.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import saboroso.saborosoburguer.entities.CustomerOrder;
 import saboroso.saborosoburguer.entities.menuItems.Drink;
 
+import java.math.BigDecimal;
+
 @Entity
 @Table
 @NoArgsConstructor
@@ -19,10 +21,13 @@ public class DrinkSale extends MenuItemSale {
     @Column
     private int quantity;
 
+    @Column (updatable = false)
+    private BigDecimal singleUnitySoldFor;
+
     public DrinkSale(CustomerOrder orderThatSold, Drink soldDrink, int quantity) {
         this.soldDrink = soldDrink;
         setOrderThatSold(orderThatSold);
         this.quantity = quantity;
-        setSingleUnitySoldFor(soldDrink.getPrice());
+        this.singleUnitySoldFor = soldDrink.getPrice();
     }
 }
