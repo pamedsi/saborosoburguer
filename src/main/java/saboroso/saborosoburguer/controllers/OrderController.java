fba9b0ed9aaab4df9739b87a2f07db9dfe7764ec
Patrick Melo <patrickabimael@yahoo.com.br>
2023-10-23 01:21:39 -0300

    implementa websocket pra pedido recebido

--- src/main/java/saboroso/saborosoburguer/controllers/OrderController.java
+++ src/main/java/saboroso/saborosoburguer/controllers/OrderController.java
@@ -1,6 +1,7 @@
 package saboroso.saborosoburguer.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 import saboroso.saborosoburguer.DTO.order.postOrder.OrderForPostDTO;
 import saboroso.saborosoburguer.models.BaseController;
@@ -10,12 +11,17 @@ import saboroso.saborosoburguer.services.OrderService;
 @RestController
 public class OrderController extends BaseController {
     private final OrderService orderService;
-    public OrderController(OrderService orderService) {
+    private final SimpMessagingTemplate template;
+
+
+    public OrderController(OrderService orderService, SimpMessagingTemplate template) {
+        this.template = template;
         this.orderService = orderService;
     }
     @PostMapping(value = "/make-order")
     public ResponseEntity<?> requestOrder(@RequestBody OrderForPostDTO orderForPostDTO) {
         orderService.makeOrder(orderForPostDTO);
+        template.convertAndSend("/topic/orders", orderForPostDTO);
         return ResponseEntity.ok(new Message("Pedido feito!", null));
     }
     @GetMapping(value = "/order-manager")
